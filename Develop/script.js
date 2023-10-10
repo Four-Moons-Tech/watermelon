09393564b3c8e7cd4a964d0ab8ae127369d6a191
Anna Moon <anna@fourmoonstech.com>
2023-10-09 20:26:50 -0500

    added prompt a 2 confirm methods to ask questions, added alert if the conditions of the prompt imput are not met, added for loop togenerate a random number. added description to README file

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,48 @@
-//Event: click button, 
-//Functions: 
-//1.dialog box includes: eventlistener in click button, prompt with questions, 
-//2. generate password includes: if statements re imputs, Math.floor(Math.random()?? to randomly choose combinations of numbers or letters, 
-      //conditional statement: 
-         //if question[0]= "yes" 
-      // then use numbers in password
-            //else do not use
-       //if question[1]= "yes" 
-          // then use numbers in password
-       //else do not use
-           ////if question[2]= "yes" 
-        // then use numbers in password
-             //else do not use
-//3. write password includes: displaying password in "#password"
-
-   generatePassword()
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-//function to open a Dialog Box 
-var dialogBox = function(){
-  var prompt = {
-    question: ['Would you like numbers in your password?','Would you like capital letters in your password?','Would you like special characters in your password?'],
-    input: ['yes','no'],
-  } 
-   for (var i=0; i <= prompt.question.lenth; i++)
-   var userChoice = window.prompt(prompt.question[0,1,2],prompt.input[0,1])
+
+var generatePassword = function(){
+  //Using a prompt method to ask a question re password lenth. 
+  var passwordLength = prompt("How many letters would you like in your password?")
+  if (passwordLength > 128 || isNaN(passwordLength) || passwordLength < 8){ // using If Statement to make sure the imput is a number
+   alert ("Start Over!")// using "alert" method to alert us to start over 1. if the user imput for password lenth is more then 128, 
+   //2. imput is not a number 3. if the password length is les that 8.
+   return "Please try again"
  }
- dialogBox() 
+ //Below I am using the confirm method for yes or no imput.
+  var passwordNumbers = confirm ("Do you want numbers in you password?")
+  var capitalLetters = confirm ("Do you want capital letters in you password?")
+  var specialCharacters = confirm ("Do you want special characters in your password?")
+  var nums = "0123456789"
+  var letters = "abcdefghijklmnopqrstuvwxyz"
+  var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var special = "!@#$%^&*()"
+  var chars = ""
+
+  //If variable passworNumbers is true, add numbers to chars variable
+  if (passwordNumbers == true){ 
+   chars += nums 
+  }
+  //If variable capitalLetters is true, add capital letters to chars variable
+  if (capitalLetters ==true){
+   chars += capital
+  }
+  //If variable specialCharacter is true, add special character to chars variable
+  if (specialCharacters ==true){
+   chars += special
+  }
+  console.log(chars) // using method log to check if my condition works
+
+   var userChoice = "" 
+   //for loops generates random number extracting substrings from chars
+    for (var i=0; i < passwordLength; i++){
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      userChoice += chars.substring (randomNumber,randomNumber +1)
+    }
+    return userChoice
+ }
+
 
 // Write password to the #password input
 function writePassword() {
